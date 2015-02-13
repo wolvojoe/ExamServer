1a2cdedaaddbb6ea1fd1ef8478c5cbbaa92445ea
Joe <joe_mason@rocketmail.com>
2015-02-13 22:12:03 +0000

    Forms

--- Admin/Modules.aspx.cs
+++ Admin/Modules.aspx.cs
@@ -12,13 +12,31 @@ public partial class Admin_Modules : System.Web.UI.Page
         lblPageTitle.Text = "Modules";
 
         GetModules();
+        GetSubjectList();
 
+        if (Request.QueryString["ID"] != null && IsPostBack == false)
+        {
+            int ModuleValue = Convert.ToInt32(Request.QueryString["ID"]);
+            GetModule(ModuleValue);
+            btnSave.Text = "Update";
+
+        }
+        else
+        {
+
+        }
+
+
+
+    }
+
+    private void GetSubjectList()
+    {
         var SubjectList = new Subject();
         dpSubject.DataSource = SubjectList.SelectAllSubjects();
         dpSubject.DataTextField = "Subject_Name";
         dpSubject.DataValueField = "pkSubject_ID";
         dpSubject.DataBind();
-
     }
 
     private void GetModules()
@@ -30,13 +48,54 @@ public partial class Admin_Modules : System.Web.UI.Page
 
     }
 
-    protected void btnSave_Click(object sender, EventArgs e)
+    private void GetModule(int ModuleID)
+    {
+        var GetModule = new Module();
+
+        GetModule.ModuleID = ModuleID;
+        GetModule.SelectModuleByID();
+
+        txtName.Text = GetModule.ModuleName;
+        chkActive.Checked = GetModule.ModuleActive;
+        dpSubject.SelectedValue = Convert.ToString(GetModule.SubjectID);
+
+    }
+
+    private void InsertModule()
     {
         var NewModule = new Module();
         NewModule.ModuleName = txtName.Text.Trim();
         NewModule.ModuleActive = chkActive.Checked;
         NewModule.SubjectID = Convert.ToInt32(dpSubject.SelectedValue);
         NewModule.InsertModule();
+    }
+
+    private void UpdateModule(int ModuleID)
+    {
+        var NewModule = new Module();
+        NewModule.ModuleID = ModuleID;
+        NewModule.SelectModuleByID();
+        NewModule.ModuleName = txtName.Text.Trim();
+        NewModule.ModuleActive = chkActive.Checked;
+        NewModule.SubjectID = Convert.ToInt32(dpSubject.SelectedValue);
+        NewModule.UpdateModule();
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+
+        if (Request.QueryString["ID"] != null)
+        {
+            int ModuleValue = Convert.ToInt32(Request.QueryString["ID"]);
+            UpdateModule(ModuleValue);
+
+        }
+        else
+        {
+            InsertModule();
+        }
+
+        GetModules();
 
     }
 
