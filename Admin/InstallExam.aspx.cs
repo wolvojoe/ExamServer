06498420017e522e1b699d3b708749a88ebf035d
Joe <joe_mason@rocketmail.com>
2015-02-18 22:17:38 +0000

    Exam updates

--- Admin/InstallExam.aspx.cs
+++ Admin/InstallExam.aspx.cs
@@ -7,12 +7,14 @@ using System.Web.UI.WebControls;
 using System.Threading;
 using System.Xml;
 using System.Xml.Linq;
+using System.IO;
 
 public partial class Admin_InstallExam : System.Web.UI.Page
 {
 
     public delegate void Worker();
-    private static Thread worker;
+    private Thread worker;
+    public string strUploadsFolder = "~/Admin/Uploads/";
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -25,7 +27,7 @@ public partial class Admin_InstallExam : System.Web.UI.Page
         if (fuUploadExam.HasFile)
         {
 
-            fuUploadExam.SaveAs(Server.MapPath("~/Admin/Uploads/") + fuUploadExam.FileName);
+            fuUploadExam.SaveAs(Server.MapPath(strUploadsFolder) + fuUploadExam.FileName);
 
             var NewThread = new Thread(() => ProcessExam(fuUploadExam.FileName));
             NewThread.Start();
@@ -39,6 +41,49 @@ public partial class Admin_InstallExam : System.Web.UI.Page
 
     public void ProcessExam(string strFileName)
     {
+        XmlDocument XMLExam = new XmlDocument();
+
+        string strxml;
+
+        strxml = LoadFile(strFileName);
+
+        if (strxml.Length > 0)
+        {
+            XMLExam.LoadXml(strxml);
+        }
+
+    }
+
+
+
+    private string LoadFile(string strFileName)
+    {
+        StreamReader textReader = new StreamReader(strUploadsFolder + strFileName);
+        string strxml;
+
+        strxml = textReader.ReadToEnd();
+
+        return strxml;
+    }
+
+    private void GetExam(XmlDocument XMLExam)
+    {
+        
+
+        XmlNode AllExams;
+        AllExams = XMLExam.DocumentElement.SelectSingleNode("Exam");
+
+
+        foreach (XmlNode ExamDetails in AllExams.SelectNodes("Exams"))
+        {
+            Exam NewExam = new Exam();
+
+            if (ExamDetails.SelectSingleNode("Name") != null)
+            {
+                NewExam.ExamName = ExamDetails.SelectSingleNode("Name").InnerText;
+            }
+        }
+
 
     }
 
